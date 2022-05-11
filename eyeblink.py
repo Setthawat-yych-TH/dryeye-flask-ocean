0da32e2aa6e2d4d727f3b03a59a6dce4739bbd2b
Setthawat Yingoyongchai <85092982+Setthawat-yych-TH@users.noreply.github.com>
2022-05-11 16:33:24 +0700

    eyelblink.py : space occur

--- eyeblink.py
+++ eyeblink.py
@@ -34,6 +34,60 @@ def eye_aspect_ratio(eye):
     return eye
  
 
+def eyeblink_halfframe (name):
+    cap = cv2.VideoCapture(os.path.join(DOWNLOAD_FOLDER,name))
+    cap.set(cv2.CAP_PROP_FPS, 10)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print(fps)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count/fps
+    duration = math.floor(duration/2)
+    print(duration)
+    
+    frame_init = 0
+    seconds = time.time() 
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('rsc/shape_predictor_68_face_landmarks.dat')
+    total = 0
+    count = 0
+    frame_step = 0
+    while True:
+        frame_step = frame_step + 1  
+        success,img = cap.read()
+        if frame_step%2 == 0:
+            continue
+        imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = detector(imgGray)
+        for face in faces:
+            landmarks = predictor(imgGray,face)
+            landmarks = face_utils.shape_to_np(landmarks)
+       
+            leftEye = landmarks[42:48]
+            rightEye = landmarks[36:42]
+            leftEye = eye_aspect_ratio(leftEye)
+            rightEye = eye_aspect_ratio(rightEye)
+            
+            eye = (leftEye + rightEye) / 2.0      
+
+            if eye<eye_check:
+                if not(leftEye > 0.25 and rightEye > 0.25):
+                    count+=1
+            else:
+                if count>=count_min and count <=count_max:
+                    print(eye)
+                    total+=1
+                count=0
+        
+        frame_init = frame_init + 1
+        timer = math.floor(frame_init/fps)
+        print('timer by frame : ' + str(timer))
+        countdown = duration-timer
+        print('countdown : ' + str(countdown))
+        realTimer = math.floor(time.time() - seconds)
+        if countdown == 0 or timer == 30:
+	        return total , realTimer , countdown
+
+
 def eyeblink (name):
     cap = cv2.VideoCapture(os.path.join(DOWNLOAD_FOLDER,name))
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -75,9 +129,10 @@ def eyeblink (name):
         timer = math.floor(frame_init/fps)
         print('timer by frame : ' + str(timer))
         countdown = duration-timer
+        realTimer = math.floor(time.time() - seconds)
         print('countdown : ' + str(countdown))
         if countdown == 0 or timer == 30:
-	        return total , timer , countdown
+	        return total , realTimer , countdown
 
 def clearFolder():
     for f in os.listdir(DOWNLOAD_FOLDER):
